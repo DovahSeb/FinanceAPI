788d24897c5f411034a2442b193b3c33c0c67075
Sebastien Larue <sebastienlar619@gmail.com>
2025-02-19 10:51:41 +0400

    Set up of the Infrastructure class

---
+++ FinanceAPI.Infrastructure/Database/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using FinanceAPI.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceAPI.Infrastructure.Database;
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<PostTitle> PostTitles { get; set; }
+}
---
+++ FinanceAPI.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FinanceAPI.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FinanceAPI.Infrastructure.Extensions;
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var defaultConnectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(defaultConnectionString));
+
+        return services;
+    }
+}
+
