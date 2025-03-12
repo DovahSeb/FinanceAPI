61221f8cd41cffb60d953e5d2234283ad9a7329f
Sebastien Larue <sebastienlar619@gmail.com>
2025-03-12 13:23:04 +0400

    Added DeactivateEmployeeHandler Test

---
+++ Tests/FinanceAPI.Application.Tests/Unit/Employee/Commands/DeactivateEmployeeCommandTests.cs
@@ -0,0 +1,24 @@
+﻿using FinanceAPI.Application.Employee;
+using FinanceAPI.Application.Employee.Commands.DeactivateEmployee;
+using NSubstitute;
+
+namespace FinanceAPI.Application.Tests.Unit.Employee.Commands;
+public class DeactivateEmployeeCommandTests
+{
+    [Fact]
+    public async Task Handle_ShouldCallDeactivateEmployee()
+    {
+        // Arrange
+        var request = new DeactivateEmployeeCommand(1);
+        var mockRepository = Substitute.For<IEmployee>();
+        var handler = new DeactivateEmployeeHandler(mockRepository);
+        var cancellationToken = CancellationToken.None;
+
+        // Act
+        await handler.Handle(request, cancellationToken);
+
+        // Assert
+        await mockRepository.Received(1).DeactivateEmployee(request.Id, cancellationToken);
+    }
+
+}
