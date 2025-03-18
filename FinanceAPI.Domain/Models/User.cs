56876319f0734c50c4b712484a048a2d4d9cbe7e
Sebastien Larue <sebastienlar619@gmail.com>
2025-03-18 14:13:58 +0400

    Added Id property to User model

--- FinanceAPI.Domain/Models/User.cs
+++ FinanceAPI.Domain/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿namespace FinanceAPI.Domain.Models;
 public class User
 {
+    public int Id { get; init; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
