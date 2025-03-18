24a579e6a5c250a6bcd211df633b7a1da0b14ae8
Sebastien Larue <sebastienlar619@gmail.com>
2025-03-18 14:35:50 +0400

    Added Users Table migration

--- FinanceAPI.Infrastructure/Database/DatabaseContext.cs
+++ FinanceAPI.Infrastructure/Database/DatabaseContext.cs
@@ -1,23 +1,19 @@
 ﻿using FinanceAPI.Domain.Models;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FinanceAPI.Infrastructure.Database;
-public class DatabaseContext : IdentityDbContext<User>
+public class DatabaseContext : DbContext
 {
     public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
 
     public DbSet<Employee> Employees { get; set; }
     public DbSet<Department> Departments { get; set; }
     public DbSet<PostTitle> PostTitles { get; set; }
+    public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<User>().ToTable("Users");
-        modelBuilder.Entity<Role>().ToTable("Roles");
-        modelBuilder.Entity<UserRole>().ToTable("UserRoles");
 
         DatabaseSeed.Seed(modelBuilder);
     }
