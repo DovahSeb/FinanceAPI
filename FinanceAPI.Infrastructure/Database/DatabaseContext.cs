431921df2a9e650d81d859dcab5ae31f8b5b6491
Sebastien Larue <sebastienlar619@gmail.com>
2025-03-20 14:08:23 +0400

    Added IdentityUser Tables

--- FinanceAPI.Infrastructure/Database/DatabaseContext.cs
+++ FinanceAPI.Infrastructure/Database/DatabaseContext.cs
@@ -1,15 +1,16 @@
 ﻿using FinanceAPI.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace FinanceAPI.Infrastructure.Database;
-public class DatabaseContext : DbContext
+public class DatabaseContext : IdentityDbContext<IdentityUser>
 {
     public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
 
     public DbSet<Employee> Employees { get; set; }
     public DbSet<Department> Departments { get; set; }
     public DbSet<PostTitle> PostTitles { get; set; }
-    public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
