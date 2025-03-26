76e4471a99a81cc24214ca14d4b275ea978093c4
Sebastien Larue <sebastienlar619@gmail.com>
2025-03-26 12:03:08 +0400

    Fixed mapping properties order

--- FinanceAPI.Infrastructure/Mapping/EmployeeMappingExtensions.cs
+++ FinanceAPI.Infrastructure/Mapping/EmployeeMappingExtensions.cs
@@ -6,6 +6,6 @@ public static class EmployeeMappingExtensions
 {
     public static EmployeeResponseDto MapToEmployeeResponseDto(this Employee employee)
     {
-        return new EmployeeResponseDto(employee.Id, employee.FirstName, employee.LastName, employee.OtherName, employee.DateOfBirth, employee.Email, employee.DateJoined, employee.Department.Name, employee.PostTitle.Name, employee.IsActive);
+        return new EmployeeResponseDto(employee.Id, employee.FirstName, employee.OtherName, employee.LastName, employee.DateOfBirth, employee.Email, employee.DateJoined, employee.Department.Name, employee.PostTitle.Name, employee.IsActive);
     }
 }
