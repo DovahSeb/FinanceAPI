67ca109bf1b17ad37ac3782c1a8e5d10e0e610af
Sebastien Larue <sebastienlar619@gmail.com>
2025-03-31 08:23:26 +0400

    Removed UserRequest and UserResponse Dto

--- FinanceAPI.Application/Auth/DTOs/UserRequestDto.cs
+++
@@ -1,2 +0,0 @@
-﻿namespace FinanceAPI.Application.Auth.DTOs;
-public record UserRequestDto(string FirstName, string LastName, string Email, string Password);
--- FinanceAPI.Application/Auth/DTOs/UserResponseDto.cs
+++
@@ -1,2 +0,0 @@
-﻿namespace FinanceAPI.Application.Auth.DTOs;
-public record UserResponseDto(int Id, string FirstName, string LastName, string Email);
