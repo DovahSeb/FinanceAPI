788bbe45cfb6bf684753a32bd630a9668f30a4cb
Sebastien Larue <sebastienlar619@gmail.com>
2025-04-01 08:55:32 +0400

    Added NotFoundException class

---
+++ FinanceAPI.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,19 @@
+﻿using FinanceAPI.Application.Common.Enums;
+
+namespace FinanceAPI.Application.Common.Exceptions;
+public class NotFoundException(string message) : Exception(message)
+{
+    public static void ThrowIfNull(object argument, EntityType entityType)
+    {
+        if(argument == null)
+        {
+            Throw(entityType);
+        }
+
+    }
+
+    public static void Throw(EntityType entityType)
+    {
+        throw new NotFoundException($"The {entityType} with the supplied id was not found");
+    }
+}
